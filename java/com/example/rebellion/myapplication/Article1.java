2193ac3b973aab2884202719e1e9e88eaab6181d
Daniel- <danielyg2904@gmail.com>
2018-11-25 01:19:45 +0700

    split basearticle to article1, login inputtype changed for password

--- java/com/example/rebellion/myapplication/Article1.java
+++ java/com/example/rebellion/myapplication/Article1.java
@@ -5,16 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class BaseArticle extends AppCompatActivity {
+public class Article1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_article);
+        setContentView(R.layout.activity_article1);
     }
-
     public void toHomepage(View view) {
-        Intent intent = new Intent(BaseArticle.this, Homepage.class);
+        Intent intent = new Intent(Article1.this, Homepage.class);
         startActivity(intent);
     }
 }
