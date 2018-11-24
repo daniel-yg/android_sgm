14d6b9be1297eb83e1fb57c54f231f5299528003
Daniel- <danielyg2904@gmail.com>
2018-11-25 01:04:05 +0700

    added static article 1

---
+++ java/com/example/rebellion/myapplication/BaseArticle.java
@@ -0,0 +1,13 @@
+package com.example.rebellion.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_article);
+    }
+}
