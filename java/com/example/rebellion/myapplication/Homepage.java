d63f067f7952dfdba0873f0aaeb2db32d9db5b98
Daniel- <danielyg2904@gmail.com>
2018-11-24 20:23:11 +0700

    finishing on homepage layout + added onclick to article

--- java/com/example/rebellion/myapplication/Homepage.java
+++ java/com/example/rebellion/myapplication/Homepage.java
@@ -1,8 +1,10 @@
 package com.example.rebellion.myapplication;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+    public void toArticle(View view) {
+        Intent intent = new Intent(Homepage.this, Article.class);
+        startActivity(intent);
     }
 }
 
